5bcb3f2da0c836ffac27893993386633603b214a
thanhlx <thanhlx1298@gmail.com>
2025-12-03 11:05:03 +0700

    Add @vercel/analytics dependency and integrate Analytics component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="vi">
       <body className={`${nunito.variable} font-sans antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
